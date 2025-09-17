cf6c5aba089127eaabc8ee0a0b483968e0766031
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-09-17 01:57:05 +0100

    adicionar página "Sobre" e atualizar navegação
    - Criada Razor Page "Sobre"
    - Adicionada action Sobre no HomeController
    - Incluída opção "Sobre" na navbar do _Layout.cshtml

--- OhLivros/OhLivrosApp/Controllers/HomeController.cs
+++ OhLivros/OhLivrosApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace OhLivrosApp.Controllers
             return View();
         }
 
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
